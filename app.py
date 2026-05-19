7d7e65596cfa4b611ed8a35e541eadaf3f020ef3
Christian Darma Setiawan <66449771+chrliem@users.noreply.github.com>
2026-05-19 21:21:51 +0700

    edit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from bertopic import BERTopic
 from huggingface_hub import snapshot_download
 
-st.set_page_config(page_title="Aviation Risk Dashboard", layout="wide")
+st.set_page_config(page_title="Aviation Risk Identification", layout="wide")
 
 @st.cache_resource
 def load_model():
@@ -15,19 +15,15 @@ except Exception as e:
     st.error(f"Gagal memuat model: {e}")
     st.stop()
 
-st.title("✈️ Aviation Risk Factor - Topic Modeling Dashboard")
+st.title("✈️ Aviation Risk Factor Identification - Topic Modeling Approach using BERTopic")
 
 user_input = st.text_area("Enter aviation incident report narrative here:", height=200)
 
-if st.button("Analyze Topic"):
+if st.button("Analyze Report"):
     if user_input:
         topics, _ = topic_model.transform([user_input])
         topic_info = topic_model.get_topic_info(topics[0])
         
         st.success(f"Risk Factor: {topic_info['CustomName'].values[0]}")
-        st.markdown("---")
-        st.write("Keywords:")
-        keywords = topic_model.get_topic(topics[0])
-        st.write([word for word, score in keywords])
     else:
         st.warning("Please enter text first.")
